a8917646e485ac32535027b2e915984e7d6f76d9
Bryan <48666047+a9874687@users.noreply.github.com>
2020-04-06 22:11:20 +0800

    Color-Code v2.5

--- js/index.js
+++ js/index.js
@@ -31,31 +31,7 @@ $( document ).ready(function() {
           }
         ]
       });
-    //--------------------------滑入位移--------------------------//
-    let img = $("#cookie-card .container ul li");
-    console.log(img);
-    console.log(img[0]);
-    let boundingBox = img[0].getBoundingClientRect();
-    console.log(boundingBox);
-    let centerX = boundingBox.left + boundingBox.width / 1;
-    let centerY = boundingBox.top + boundingBox.height / 1;
-    $("#cookie-card .container ul").mousemove(function (e) {
-        // console.log(e);
-        let { pageX, pageY } = e;
-        // console.log({ pageX, pageY });
-        let [distanceX, distanceY] = [centerX - pageX, centerY - pageY];
-        // console.log([distanceX, distanceY]);
-
-        img.css({
-            "transform": `rotateX(${distanceX / 20}deg) rotateY(${distanceY / 20}deg) translateZ(40px)`
-        });
-        $("#cookie-card .container ul li").css({
-            "transform": `translateX(${distanceX / 20}px) translateY(${distanceY / 20}px)`
-        });
-        $("#cookie-card .container ul li").css({
-            "transform": `translateX(${-distanceX / 20}px) translateY(${-distanceY / 20}px)`
-        });
-    });
+   
 });
 //--------------------------點擊移動指定區塊--------------------------//
 $(".scrollTop").click(function(e){
